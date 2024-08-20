e5fffecbe875d5e0df894b6bf01a6ffdde161829
joaopivaa <joao.piva@aluno.ifsp.edu.br>
2024-08-20 17:23:47 -0300

    commit

--- Aula4/Beecrowd.sql
+++ Aula4/Beecrowd.sql
@@ -104,3 +104,105 @@ SELECT c.name, ROUND(((s.math*2) + (s.specific*3) + (s.project_plan*5))/10.0,2) 
 FROM candidate c
 INNER JOIN score s ON s.candidate_id = c.id
 ORDER BY avg DESC ;
+
+-- EXIBIR A TEMPERATURA E NÚMERO DE REGISTROS CORRESPONDENTES
+SELECT temperature, COUNT(mark) AS number_of_records
+FROM records
+GROUP BY mark, temperature ORDER BY mark ;
+
+-- EXIBIR O NOME E O DIA DO MÊS EM QUE OS CLIENTES DEVEM PAGAR SUAS PARCELAS
+SELECT name, CAST((EXTRACT(DAY FROM payday)) as INT) AS day FROM loan ;
+
+-- EXIBIR 'PODIUM: ' PARA OS 3 PRIMEIROS COLOCADOS, E 'DEMOTED:' PARA OS DOIS ÚLTIMOS DA LIGA
+SELECT CONCAT('Podium: ', team) AS name 
+FROM league WHERE position IN (SELECT position FROM league ORDER BY position LIMIT 3)
+            UNION ALL
+SELECT CONCAT('Demoted: ', team) AS name
+FROM league WHERE position IN (SELECT position FROM league ORDER BY position DESC LIMIT 2);
+
+-- EXIBIR O NOME DOS PRODUTOS E DA CATEGORIA DOS PRODUTOS QUE TENHAM A QTD MAIOR QUE 100 E DOS SEGUINTES CODS
+SELECT p.name, c.name 
+FROM products p
+INNER JOIN categories c ON c.id = p.id_categories
+WHERE p.amount > 100 AND c.id IN (1, 2, 3, 6, 9)
+ORDER BY c.id;
+
+-- EXIBIR TODOS OS RICHARDS DAS DIMENSÕES C875 E C774 JUNTO DA PROBABILIDADE DE EXISTÊNCIA (FATOR N)
+SELECT lr.name, ROUND((lr.omega * 1.618), 3) AS "Fator N"
+FROM life_registry lr
+INNER JOIN dimensions d ON d.id = lr.dimensions_id
+WHERE d.name IN ('C875', 'C774') AND lr.name LIKE '%Richard%' 
+ORDER BY lr.omega ;
+
+-- APLICAR A MÁSCARA PARA TODOS OS CPF'S DOS CLIENTES
+SELECT 
+    CONCAT(
+        SUBSTRING(cpf, 1, 3), '.', 
+        SUBSTRING(cpf, 4, 3), '.', 
+        SUBSTRING(cpf, 7, 3), '-', 
+        SUBSTRING(cpf, 10, 2)
+    ) AS CPF
+FROM 
+    natural_person;
+
+-- EXIBIR O ANO E NOME DOS CLIENTES QUE MOVIMENTARAM PACOTES AZUIS OU DO ANO DE 2015, COM OS ENDEREÇOS SENDO DIFERENTES DE TAIWAN
+SELECT p.year, sender.name AS sender, receiver.name AS receiver
+FROM packages p
+INNER JOIN users AS sender ON p.id_user_sender = sender.id
+INNER JOIN users AS receiver ON p.id_user_receiver = receiver.id
+WHERE (p.color = 'blue' OR p.year = 2015)
+AND (sender.address != 'Taiwan' AND receiver.address != 'Taiwan')
+ORDER BY p.year DESC ;
+
+-- EXIBIR O ID E NOME DOS CLIENTES SEM LOCAÇÃO COM ORDENAÇÃO POR ID
+SELECT c.id, c.name FROM customers c 
+LEFT JOIN locations l ON c.id = l.id_customers
+WHERE l.id_customers IS NULL
+ORDER BY c.id ;
+
+-- EXIBIR O NOME DOS ALUNOS APROVADOS E SUA NOTA
+SELECT CONCAT('Approved: ', name) AS name, grade FROM students
+WHERE grade >= 7
+ORDER BY grade DESC ; 
+
+-- EXIBIR O ADVOGADO COM MAIS E MENOS CLIENTES, E A MÉDIA DE CLIENTES TOTAL
+SELECT name, customers_number
+FROM lawyers
+WHERE customers_number = (SELECT MAX(customers_number) FROM lawyers)
+    UNION ALL
+SELECT name, customers_number
+FROM lawyers
+WHERE customers_number = (SELECT MIN(customers_number) FROM lawyers)
+    UNION ALL
+SELECT 'Average' AS name, ROUND(AVG(customers_number), 0) AS customers_number
+FROM lawyers;
+
+-- EXIBIR NOME DO DEPARTAMENTOS E SUAS DIVISÕES, ALÉM DA MÉDIA SALARIAL E MAIOR SALÁRIO DE CADA DIVISÃO
+SELECT
+    d.nome AS Departamento,
+    div.nome AS Divisao,
+    ROUND(AVG(tsalario.salario - tdescontos.descontos), 2) AS media,
+    ROUND(MAX(tsalario.salario - tdescontos.descontos), 2) AS maior
+FROM departamento d
+INNER JOIN divisao div ON d.cod_dep = div.cod_dep
+INNER JOIN empregado emp ON div.cod_divisao = emp.lotacao_div
+INNER JOIN ( 
+             SELECT emp.matr, COALESCE(SUM(v.valor), 0) AS salario
+             FROM empregado emp
+             LEFT JOIN emp_venc ON emp.matr = emp_venc.matr
+             LEFT JOIN vencimento v ON emp_venc.cod_venc = v.cod_venc
+             GROUP BY emp.matr
+           ) 
+             AS tsalario ON emp.matr = tsalario.matr
+INNER JOIN (
+             SELECT emp.matr, COALESCE(SUM(desconto.valor), 0) AS descontos
+             FROM empregado emp
+             LEFT JOIN emp_desc ON emp.matr = emp_desc.matr
+             LEFT JOIN desconto ON emp_desc.cod_desc = desconto.cod_desc
+             GROUP BY emp.matr
+           ) 
+             AS tdescontos ON emp.matr = tdescontos.matr
+GROUP BY div.cod_divisao, div.nome, d.nome
+ORDER BY media DESC;
+
+-- EXIBIR NOME DO DEPARTAMENTO, NOME DA DIVISÃO COM A MAIOR MÉDIA SALARIAL E A MÉDIA DA DIVISÃO
